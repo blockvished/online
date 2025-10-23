97f3ad280ba5ab50f254c4476d9907f5f8f276e5
blockvished <jcmvishael@gmail.com>
2025-10-23 07:52:58 +0000

    usernames for the users

--- hardhat-example/contracts/SealCrypt.sol
+++ hardhat-example/contracts/SealCrypt.sol
@@ -2,16 +2,19 @@
 pragma solidity ^0.8.0;
 
 contract SafeCrypt {
-
     address public owner;
     mapping(address => bool) public isAdmin;
 
-    mapping(address => mapping(uint => string)) public cids;
-    mapping(address => uint) public cidCount;
+    mapping(address => string) public usernames;
+    mapping(string => address) public usernameToAddress;
+
+    mapping(address => mapping(uint256 => string)) public cids;
+    mapping(address => uint256) public cidCount;
 
     event AdminAdded(address indexed admin);
     event AdminRemoved(address indexed admin);
     event CIDAdded(address indexed user, string cid, address indexed addedBy);
+    event UsernameSet(address indexed user, string username);
 
     constructor() {
         owner = msg.sender;
@@ -42,19 +45,40 @@ contract SafeCrypt {
     }
 
     function addCID(address user, string calldata cid) external onlyAdmin {
-        uint index = cidCount[user] + 1;
+        uint256 index = cidCount[user] + 1;
         cids[user][index] = cid;
         cidCount[user] = index;
 
         emit CIDAdded(user, cid, msg.sender);
     }
 
-    function getCID(address user, uint index) external view returns (string memory) {
+    function getCID(
+        address user,
+        uint256 index
+    ) external view returns (string memory) {
         require(index <= cidCount[user], "Index out of bounds");
         return cids[user][index];
     }
 
-    function getCIDCount(address user) external view returns (uint) {
+    function getCIDCount(address user) external view returns (uint256) {
         return cidCount[user];
+    }
+
+    function setUsername(string calldata username) external {
+        require(bytes(username).length > 0, "Username cannot be empty");
+        require(
+            usernameToAddress[username] == address(0),
+            "Username already taken"
+        );
+
+        string memory oldUsername = usernames[msg.sender];
+        if (bytes(oldUsername).length > 0) {
+            delete usernameToAddress[oldUsername];
+        }
+
+        usernames[msg.sender] = username;
+        usernameToAddress[username] = msg.sender;
+
+        emit UsernameSet(msg.sender, username);
     }
 }
